30dde3b78c9b6ba728d5ee9a38e822defff1226d
Damian Grembocki <damian.grembocki@o2.pl>
2023-12-19 18:11:04 +0100

    Ecommerce app Scaffold

---
+++ lib/shop_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+import 'components/shoe_tile.dart';
+import 'models/shoe.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const[
+              Text(
+                  'Search',
+                  style: TextStyle(color:  Colors.grey)),
+              Icon(
+                  Icons.search,
+                  color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25.0),
+          child: Text(
+              'everyone flies.. some fly longer than others',
+              style: TextStyle(color: Colors.grey[600])
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: const [
+              Text(
+                'Hot Picks 🔥',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              Text(
+                'See all',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 10,),
+        Expanded(child: ListView.builder(
+            itemBuilder: (context, index){
+              Shoe shoe = Shoe(
+                  name: 'Air Fordan',
+                  price: '230',
+                  imagePath: 'lib/images/shoe1.png',
+                  description: 'Fake good shoe'
+              );
+              return ShoeTile(
+                shoe: shoe,
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
